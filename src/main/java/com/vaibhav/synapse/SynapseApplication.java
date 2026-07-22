26aa9a430a37f3daf68a547094c75199c110f02e
vaibsoni <soni.vaibhav48@gmail.com>
2026-07-23 00:47:47 +0530

    Initial commit.

---
+++ src/main/java/com/vaibhav/synapse/SynapseApplication.java
@@ -0,0 +1,13 @@
+package com.vaibhav.synapse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SynapseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SynapseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vaibhav/synapse/SynapseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vaibhav.synapse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynapseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
