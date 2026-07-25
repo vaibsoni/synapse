0a7b43cc9b005126b42daad1572231c663a0c43b
vaibsoni <soni.vaibhav48@gmail.com>
2026-07-26 00:12:59 +0530

    configured flyway,actuator and exception Handling.

---
+++ src/main/java/com/vaibhav/synapse/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,7 @@
+package com.vaibhav.synapse.common.exception;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+}
